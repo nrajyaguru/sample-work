683443f7bf1d9aa05a8a74df8d010ae9b44d8fab
nrajyaguru <141554251+nrajyaguru@users.noreply.github.com>
2024-08-08 16:44:36 +0530

    Feature flag library
    initial draft

--- Materials/Atlassian/Javascript coding/Coding samples/Build a feature flag library/src/featureFlag.js
+++ Materials/Atlassian/Javascript coding/Coding samples/Build a feature flag library/src/featureFlag.js
@@ -2,6 +2,23 @@ class FeatureFlag {
     constructor() {
         this.flags = {}
     }
+
+    enableFeature(feature, condition = null) {
+        this.flags[feature] = { enabled: true, condition };
+    }
+
+    disableFeature(feature) {
+        this.flags[feature] = { enabled: false, condition: null };
+    }
+
+    isFeatureEnabled(feature, context = null) {
+        const flag = this.flags[feature];
+        if (!flag) return false;
+        if (flag.condition) {
+            return flag.condition(context);
+        }
+        return flag.enabled;
+    }
 }
 
 module.exports = FeatureFlag;
